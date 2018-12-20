4def7f289ffc23d2b84f0a08f8e10f0a66384ea6
prescottbreeden <prescott.breeden@gmail.com>
2018-12-20 00:52:58 -0800

    added extra js class to play with

--- algos.js
+++ algos.js
@@ -10,12 +10,3 @@ function reverseArray(arr) {
 var array1 = [1,2,3,4,5];
 console.log(reverseArray(array1));
 
-function (arr) {
-  for(var i = 0; i < arr.length/2; i++) {
-    var temp = arr[i];
-    arr[i] = arr[arr.length-1-i];
-    arr[arr.length-1-i] = temp;
-  }
-  return arr;
-}
-
